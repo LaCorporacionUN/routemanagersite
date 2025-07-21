4f99c7c0f786c85735e5c0d55cb90e8f0d4769af
paranimias <ndceliso@unal.edu.co>
2025-07-20 21:33:34 -0500

    changed favicons

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753062774',resources: [
+    cacheName: 'chirpy-1753065117',resources: [
       '/routemanagersite/assets/css/jekyll-theme-chirpy.css',
       '/routemanagersite/',
       
